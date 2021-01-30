3a9a935b266fda220b79085f874a5880ec787f5a
Jose Fandino <piolinfan07@gmail.com>
2021-01-30 17:56:05 -0500

    Copy

---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormTodo from './FormTodo';
+import TaskList from './TaskList';
+const Container = () =>{
+    return (
+        <div>
+            container!
+            <FormTodo/>
+            <TaskList/>
+        </div>
+    )
+}
+export default Container;
