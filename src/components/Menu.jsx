3ae917583f9ef1f5e236a309b5f1406bf48e5e92
luis-rios <vvrhz11@gmail.com>
2021-02-21 11:51:09 -0600

    cambios

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCalendarAlt, faClipboardCheck, faEdit, faFileAlt, faHome} from '@fortawesome/free-solid-svg-icons'
 import '../App.css';
-
 
 
 export default class Menu extends React.Component{
@@ -11,19 +12,16 @@ constructor(){
 }
 
     render(){
-        return(
-           
-                <nav className="menu">
-                    <ul>
-                        <li> <a> Inicio</a> </li>
-                        <li> <a>Pendientes  </a> </li>
-                        <li> <a> Realizadas</a> </li>
-                    </ul>
-
-                </nav>
-
-
-          
+        return(                   
+            <div className="menu">
+                <ul>
+                    <li> <button> <FontAwesomeIcon icon={faHome} style={{paddingRight:'10px'}}/>Inicio</button></li>
+                    <li> <button> <FontAwesomeIcon icon={faFileAlt} style={{paddingRight:'10px'}} /> Pendientes </button></li>
+                    <li><button> <FontAwesomeIcon icon={faCalendarAlt} style={{paddingRight:'10px'}} /> Proxima semana </button> </li>
+                    <li> <button> <FontAwesomeIcon icon={faClipboardCheck} style={{paddingRight:'10px'}} /> Realizadas </button></li>
+                    <li><button> <FontAwesomeIcon icon={faEdit} style={{paddingRight:'10px'}} /> Notas </button></li>
+                </ul> 
+            </div>                         
         );
     }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,22 @@
 import React from 'react';
 import Menu from '../components/Menu';
-
 import Container from '../components/Container';
 import Card from '../components/Card';
-import '../App.css';
-import '../styles/Menu.css';
 import Image from '../assets/img/bg.jpg';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Calendario from '../components/Calenario';
-
-
+import Task from '../assets/img/task.jpg'
+import '../styles/Menu.css';
 
 export default function Home(){
-
     return(
-
-        <div>
+        <div className="Home" style={{backgroundImage:`url(${Task})`}}>
             <section className='presentacion' style={{backgroundImage:`url(${Image})`}}>
                 <h2>Welcome to your task</h2>
             </section>
-            <Menu/>
-            <Container/>
-            <Card/>
-
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Dropdown Button
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-
-     
+            <div className="container">
+                <Menu/>
+                <Container/>
+                <Card/>     
+            </div>
         </div>
-
-
     );
 }
